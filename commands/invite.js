8a08ce1a8da2936d0e5336d1eb3e559c39ff49e6
oo273825 <97148927+oo273825@users.noreply.github.com>
2022-12-07 20:41:07 +0800

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = {
   name: "invite",
-  description: "To invite me to your server",
+  description: "邀請張先生去你家玩",
   usage: "",
   permissions: {
     channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
